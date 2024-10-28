2d06bd6fbf43fc297ad7eef414dd80ec82961abc
Kolminch <122073398+Kolminch@users.noreply.github.com>
2024-10-28 19:13:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN,
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 
